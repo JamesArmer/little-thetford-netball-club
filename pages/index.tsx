4d9542deb48653cdfa4d9891715f93948a654c15
James Armer <jamesarmer2@gmail.com>
2022-09-03 14:00:17 +0100

    Home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,54 +5,47 @@ import React from "react";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className="flex w-full flex-1 flex-col items-center justify-center px-20 py-16 text-center">
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex w-full flex-1 flex-col items-center justify-center px-20 py-4">
+        <h1 className="text-4xl text-center font-bold text-blue-500 py-4">
+          Welcome to Little Thetford Netball Club!
+        </h1>
+        <div className="w-3/5 py-2">
+          <h2 className="text-2xl font-bold">
+            Little Thetford Netball Club wants you!
+          </h2>
+          <p className="text-xl">
+            Little Thetford Netball Club welcomes new adult players (16+) of all
+            abilities. We are a friendly club who offer the opportunity to play
+            competitive netball within our teams who compete in the Cambridge
+            and District Netball League. Although we have a competitive streak,
+            we also appreciate keeping fit and having fun and a number of our
+            members come along for training sessions only.
+          </p>
+        </div>
+        <div className="w-3/5 py-2">
+          <h2 className="text-2xl font-bold">Training</h2>
+          <p className="text-xl">
+            We train every Tuesday at the courts at Little Thetford sports
+            facilities from 19:00 until 20:00. We welcome new players who may be
+            returning to the sport, have moved to the area, complete newbies or
+            those who are curious to give it a go. <br />
+            <i>(Check out our &apos;News&apos; page for more information)</i>
+          </p>
+        </div>
+        <div className="w-3/5 py-2">
+          <h2 className="text-2xl font-bold">Training</h2>
+          <p className="text-xl">
+            Little Thetford Netball Club wants everybody to enjoy training and
+            remain safe whilst doing so, we therefore ask all players to follow
+            some basic netball etiquette.
+            <br /> Please:
+          </p>
+          <ul className="text-xl list-disc pl-10">
+            <li>Ensure your nails are short</li>
+            <li>Remove all jewellery</li>
+            <li>Tie long hair back</li>
+            <li>Wear appropriate attire and footwear</li>
+          </ul>
         </div>
       </div>
     </Layout>
