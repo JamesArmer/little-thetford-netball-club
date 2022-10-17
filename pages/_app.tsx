b7f33d0a8cdcf86fab602beb1d5dc1c2a36b10c1
James Armer <jamesarmer2@gmail.com>
2022-10-17 20:03:09 +0100

    Add meta tags for SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Little Thetford Netball Club</title>
         <link rel="icon" href={favicon.src} />
+        <meta
+          name="description"
+          content="Little Thetford Netball Club welcomes players of all abilities and
+            are always delighted to have new members."
+        />
+        <meta name="og:type" content="website" />
+        <meta name="og:title" content="Little Thetford Netball Club" />
+        <meta
+          name="og:description"
+          content="Little Thetford Netball Club welcomes players of all abilities and
+            are always delighted to have new members."
+        />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
